a1da82e3865b28572ec43019e23e36f84a305de0
Riyaz Ali <54612827+aayrm5@users.noreply.github.com>
2020-09-04 14:34:49 +0530

    Using Urllib to retrieve location information in JSON format.

--- geojson.py
+++ geojson.py
@@ -1,16 +1,30 @@
 import urllib.request, urllib.parse, urllib.error
 import json
+import ssl
 
-serviceurl = 'http://maps.googleapis.com/maps/api/geocode/json?'
+api_key = False
+
+if api_key is False:
+    api_key = 42
+    service_url = 'http://py4e-data.dr-chuck.net/json?'
+else:
+    service_url = 'http://maps.googleapis.com/maps/api/geocode/json?'
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 while True:
     address = input('Enter Location: ')
     if len(address) < 1: break
 
-    url = serviceurl + urllib.parse.urlencode({'address': address})
+    params = dict()
+    params['address'] = address
+    if api_key is not False: params['key'] = api_key
+    url = service_url + urllib.parse.urlencode(params)
 
     print("Retrieving", url)
-    uh = urllib.request.urlopen(url)
+    uh = urllib.request.urlopen(url,context=ctx)
     data = uh.read().decode()
     print("Retrieved",len(data), 'characters')
 
